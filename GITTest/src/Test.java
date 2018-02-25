faecba17de2ae6cc5353fa8c8a870a11d85467aa
User <User@CHNSKC0096L>
2018-02-25 19:22:33 +0530

    commit

---
+++ GITTest/src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		login();
+		
+		
+	}
+	
+	
+	public static void login()
+	
+	
+	{
+		
+		System.out.println("patient reorcd");
+	}
+
+}
---
+++ Jenkins/src/Testdata.java
@@ -0,0 +1,20 @@
+
+public class Testdata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+login();
+
+		
+	}
+		public static  void  login()
+		
+		{
+			
+			System.out.println("Test data for the Patient record");
+			
+		}
+		
+		
+	}
+
